8894a3ade4f092afed7f0f61ac64f211400c688a
Hemanthpai5204 <137260723+Hemanthpai5204@users.noreply.github.com>
2025-11-20 16:49:17 +0530

    ss

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
-import pymysql
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here'  # Change this to a random secret key
 
-# MySQL database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:jeevanpai2006@localhost/simplee'
+# SQLite database configuration (works with Vercel)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
