d3e12449682cb5d189c313fbdfa3610e6cdfc2ab
Hemanthpai5204 <137260723+Hemanthpai5204@users.noreply.github.com>
2025-11-20 17:36:33 +0530

    ss

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'  # Change this in production
